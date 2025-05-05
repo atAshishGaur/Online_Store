8441a0efaf44fd950500c4feb3dd5199373cfb2e
ashish.gaur <ashish.gaur@aieze.in>
2025-05-05 12:07:33 +0530

    commit4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/main';
 import Header from './components/header';
 import Second from './components/second';
